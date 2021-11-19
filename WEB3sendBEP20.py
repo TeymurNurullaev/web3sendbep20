f8cf1f4de187c6ae7aee2e41b75bbf855086db71
Teymur Nurullaev <cgunreal@yandex.ru>
2021-11-20 01:14:09 +0300

    Create WEB3sendBEP20.py

---
+++ WEB3sendBEP20.py
@@ -0,0 +1,33 @@
+from web3 import Web3
+import json
+
+
+def sendBep20(send, from_adr, to_adr, key):
+    bsc = "https://bsc-dataseed1.binance.org/"
+    w3 = Web3(Web3.HTTPProvider(bsc))
+    
+    abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]')
+    contractAddress = w3.toChecksumAddress("0xf1b752b33b670ccfefa6bb9204933d2aa3811500")
+    contract = w3.eth.contract(address=contractAddress, abi=abi)
+    nonce = w3.eth.getTransactionCount(w3.toChecksumAddress(from_adr))  
+    amount = send * 100000000
+    
+    # Build a transaction that invokes this contract's function, called transfer
+    token_txn = contract.functions.transfer(w3.toChecksumAddress(to_adr), amount,).buildTransaction({
+        'chainId': 56,
+        'gas': 100000,
+        'gasPrice': w3.toWei('10', 'gwei'),
+        'nonce': nonce,
+        })
+    
+    signed_txn = w3.eth.account.signTransaction(token_txn, private_key=key)
+    w3.eth.sendRawTransaction(signed_txn.rawTransaction) 
+    
+    
+
+send = 10
+token_from = "0x9..."
+token_to =  "0x3..."
+token_to_private_key = "..."
+
+sendBep20(send, token_from, token_to, token_to_private_key)
